6354a0e0e822f70e8035289ddf4953a2daa441d5
lunarcentric <hcmarcus@MarcusoLaptop.local>
2014-03-12 03:33:10 -0500

    Began work on randomizing time between enemy spawns

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,6 +5,7 @@ public class Spawner : MonoBehaviour {
 	
 	public float spawnTime = 5f;		// The amount of time between each spawn.
 	public float spawnDelay = 3f;		// The amount of time before spawning starts.
+	public bool randomizeTime = true;	// Whether the time between spawns should be random or not.
 	public GameObject[] enemies;		// Array of enemy prefabs.
 	
 	void Start ()
@@ -15,8 +16,9 @@ public class Spawner : MonoBehaviour {
 	
 	void Spawn ()
 	{
-		// Instantiate a random enemy.
+		// Select a random enemy from the array.
 		int enemyIndex = Random.Range(0, enemies.Length);
+    	// Instantiate a random enemy.
 		Instantiate (enemies [enemyIndex], transform.position, enemies [enemyIndex].transform.rotation);
 	}
 }
